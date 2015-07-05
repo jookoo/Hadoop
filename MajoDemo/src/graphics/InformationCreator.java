ba6010cc3331c57ebf82c3b864fbf37ed7dceab8
Joshua Kornfeld <jokornfeld@gmail.com>
2015-07-05 14:03:51 +0200

    -rc2

--- MajoDemo/src/graphics/InformationCreator.java
+++ MajoDemo/src/graphics/InformationCreator.java
@@ -218,7 +218,7 @@ public class InformationCreator {
 				final SessionLine sl = new SessionLine(line);
 				x.add(sl);
 				line = br.readLine();
-				// begrenz die eingelesen Zeilen
+				// begrenzt die eingelesen Zeilen
 				if(count > 10000) {
 					break einlesen;
 				}
